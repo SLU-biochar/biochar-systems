515febefe6a7078bb27e3dba19b0aea51259861a
Elias Sebastian Azzi <elias_azzi@hotmail.fr>
2021-10-13 12:26:57 +0200

    Update Week 2 October

--- bw2widgets/widget_plot_types.js
+++ bw2widgets/widget_plot_types.js
@@ -1,13 +1,15 @@
 // contains various plotting functions using d3.js
 // with variables to select in which html div to insert the figures & sliders
 
+// responsive SVG : https://medium.com/@louisemoxy/a-simple-way-to-make-d3-js-charts-svgs-responsive-7afb04bc2e4b 
+
 function write_widget(id_figure, id_parameter, param_data, switch_data, algebraic_eq, plot_type){
 	
 	document.getElementById(id_parameter).innerHTML='';
 	//console.log(param_data);
 	
 	// write parameter sliders
-	var html_txt = '<p>Click on the parameters to change their values</p>'
+	var html_txt = '<p>Click on the parameters to change their values. Expand all. Collapse all. </p>'
 	html_txt += '<div class="listParam">';
 	html_txt += write_sliders(id_figure, id_parameter, param_data, switch_data, algebraic_eq, plot_type);
 	html_txt += '</div>';
@@ -39,25 +41,32 @@ function write_sliders(id_figure, id_parameter, param_data, switch_data, algebra
 	html_txt = '';
 	for(i in context[param_data]['name']){
 		
-		var name = context[param_data]['name'][i]; 
-		
+		var name = ( (context["parameter_data"].hasOwnProperty("prettyName")) ? context[param_data]['prettyName'][i] : context[param_data]['name'][i] ); // bw2 name or prettyName if it exists and is not null 
+		var nameID = context[param_data]['name'][i];
+		var description = ( (context["parameter_data"].hasOwnProperty("description")) ? context[param_data]['description'][i] : '' );
+		var unit = ( (context["parameter_data"].hasOwnProperty("unit")) ? context[param_data]['unit'][i] : '' );
+
 		html_txt += ' <section class="accordion"> ' ;
-		html_txt += ' <input type="checkbox" name="collapse" id="handle_'+name+'" > ';
-		html_txt += ' <h5 class="handle"> <label for="handle_'+name+'"> '+name+'</label></h5>'; // replace by pretty name + tooltip description
+		html_txt += ' <input type="checkbox" name="collapse" id="handle_'+nameID+'" > ';
+		html_txt += ' <h5 class="handle"> <label for="handle_'+nameID+'"> '+name; // prettyName if it exists
+		    
+		html_txt += ' <a class="glossary-tooltip" title="'+description+'" data-toggle="tooltip" data-placement="top" aria-label="'+description+'"> &#x24D8; </a>'; // tooltip description in the info symbolo
+		html_txt += ' </label></h5>'; 
 		html_txt += ' <div class="content">';
 		
 		if(context[param_data]['uncertainty type'][i] == 'switch'){
-			var nbOptions = context[switch_data][name]['options'].length
-			html_txt += ' <select name="select_'+name+'" onchange="updateWidget(this.value, this.name, \''+id_figure+'\', algebraic_equation_f, \''+plot_type+'\', \''+switch_data+'\');" >' ;
+			var nbOptions = context[switch_data][nameID]['options'].length
+			html_txt += ' <select name="select_'+nameID+'" onchange="updateWidget(this.value, this.name, \''+id_figure+'\', algebraic_equation_f, \''+plot_type+'\', \''+switch_data+'\');" >' ;
 			for(let j=0; j < nbOptions; j++){
-				html_txt += ' <option value='+j+'>'+context[switch_data][name]['options'][j]+'</option> ';
+				html_txt += ' <option value='+j+'>'+context[switch_data][nameID]['options'][j]+'</option> ';
 			}
 			html_txt += '</select>' ;						
 		}else{
 			// html_txt += '  Minimum: '+ context[param_data]['minimum'][i]+' ';
-			html_txt += ' <input type="range" name="range_'+name+'" min="'+context[param_data]['minimum'][i]+'" max="'+context[param_data]['maximum'][i]+'" step = "'+(context[param_data]['maximum'][i] - context[param_data]['minimum'][i])/100+'"  ';
+			html_txt += ' <input type="range" name="range_'+nameID+'" min="'+context[param_data]['minimum'][i]+'" max="'+context[param_data]['maximum'][i]+'" step = "'+(context[param_data]['maximum'][i] - context[param_data]['minimum'][i])/100+'"  ';
 			html_txt += ' onchange="updateWidget(this.value, this.name, \''+id_figure+'\', algebraic_equation_f, \''+plot_type+'\', \''+switch_data+'\');" >' ;
-			html_txt += ' <input type="text" id="txt_'+name+'" value="'+context[param_data]['amount'][i]+'" maxlength="4" size="4" onchange="updateWidget(this.value, this.id, \''+id_figure+'\', algebraic_equation_f, \''+plot_type+'\', \''+switch_data+'\');" >' ;			
+			html_txt += ' <input type="text" id="txt_'+nameID+'" value="'+context[param_data]['amount'][i]+'" maxlength="4" size="4" onchange="updateWidget(this.value, this.id, \''+id_figure+'\', algebraic_equation_f, \''+plot_type+'\', \''+switch_data+'\');" >' ;			
+			html_txt += ' in: <i>' + unit+'</i>';
 		}
 		html_txt += ' </div> </section>';			
 	}
@@ -102,6 +111,17 @@ function updateWidget(val, NameOrId, id_figure, algebraic_eq, plot_type, switch_
 	}	
 }
 
+/* 
+	Variables useful across document
+*/
+
+var nb2txt_2c = d3.format(",.2r"),
+	nb2txt_3c = d3.format(",.3r");
+
+/*
+	Plotting functions: stackedbar, waterfall
+*/
+
 function plot_stackedbar(id_figure, algebraic_values){
 	// https://www.d3-graph-gallery.com/graph/barplot_stacked_highlight.html
 	// https://www.d3-graph-gallery.com/graph/custom_legend.html 
@@ -110,47 +130,69 @@ function plot_stackedbar(id_figure, algebraic_values){
 	// https://observablehq.com/@d3/diverging-stacked-bar-chart 
 
 	console.log("algebraic values", algebraic_values);
-
+	
 	// set the dimensions and margins of the graph
 	var margin = {top: 10, right: 30, bottom: 20, left: 50},
-		width = 460 - margin.left - margin.right,
-		height = 400 - margin.top - margin.bottom;
+    legend_width = 150,
+    width = 500 - margin.left - margin.right - legend_width,
+    height = 325 - margin.top - margin.bottom;
+    legend_height = height + margin.top + margin.bottom;
+	
+	var xoffset_legend = width-margin.left-margin.right;
+	var yoffset_legend = -8*margin.top;
 
+	var svgWidth = width + margin.left + margin.right + legend_width,
+		svgHeight = height + margin.top + margin.bottom;
+	
 	// append the svg object to the body of the page
 	var svg = d3.select("#"+id_figure)
 	  .append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
+		//.attr("width", width + margin.left + margin.right)
+		//.attr("height", height + margin.top + margin.bottom)
+		.attr("viewBox", "0 0 "+svgWidth+" "+svgHeight+"")
 	  .append("g")
 		.attr("transform",
 			  "translate(" + margin.left + "," + margin.top + ")");
 
 	var groups = ['Willow1', 'Will2', "Will3"]; // Name of the product - not contained in data yet...
 	var stages = Object.keys(algebraic_values[0]) ; // the keys of the data
+	stages = stages.slice(1);
+	// stages.pop("group"); // remove the col name "group"
+	console.log("stages", stages);	
 	var stackedData= d3.stack()
 						.keys(stages) // which keys to consider
 						.offset(d3.stackOffsetDiverging) // strategy for negative/positive values
 						(algebraic_values); 	// apply to the stack generator to the data
 
-	console.log("stacked data", stackedData);
-
-		// Add X axis
-		var x = d3.scaleBand()
-		.domain(algebraic_values.map(function(d) { return d.group; }) ) 
-		.range([0, width])
-		.padding([0.2])
-
-		svg.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x).tickSizeOuter(0));
-
-		function stackMin(serie) {
-		return d3.min(serie, function(d) { return d[0]; });
+	console.log("stackedData", stackedData);	
+	function calcNet(obj) {
+		var sum = 0;
+		for( var el in obj ) {
+		  if( obj.hasOwnProperty( el ) && el != "group" ) { sum += parseFloat( obj[el] ); }
 		}
+		return sum;
+	  }
+	var netValues = algebraic_values.map(x => [x.group, calcNet(x)] );
+	console.log("NetValues", netValues);
+
+	// Add X axis
+	var x = d3.scaleBand()
+	.domain(algebraic_values.map(function(d) { return d.group; }) ) 
+	.range([0, width])
+	.padding([0.2])
+
+	svg.append("g")
+	.attr("transform", "translate(0," + height + ")")
+	.call(d3.axisBottom(x).tickSizeOuter(0));
+
+	function stackMin(serie) {
+	return d3.min(serie, function(d) { return d[0]; });
+	}
 		
 	function stackMax(serie) {
 	return d3.max(serie, function(d) { return d[1]; });
 	}
+
 	// Add Y axis
 	var y = d3.scaleLinear()
 	.domain([ 1.2*d3.min(stackedData, stackMin), 1.2*d3.max(stackedData, stackMax) ])
@@ -158,6 +200,15 @@ function plot_stackedbar(id_figure, algebraic_values){
 
 	svg.append("g")
 	.call(d3.axisLeft(y));
+	  // Add 0 line
+	  var y0 = y(0);
+	  svg.append("line")
+			.attr("class", "starting-line")
+			.attr("x1", 0)
+			.attr("y1", y0)
+			.attr("x2", width)
+			.attr("y2", y0)
+			.style("stroke", "black")
 
 	// color palette = one color per subgroup
 	var color = d3.scaleOrdinal()
@@ -201,11 +252,70 @@ function plot_stackedbar(id_figure, algebraic_values){
         .attr("x", function(d) { return x(d.data.group); })
         .attr("y", function(d) { return y(d[1]); })
         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-        .attr("width",x.bandwidth())
+        .attr("width", x.bandwidth())
         .attr("stroke", "grey")
       .on("mouseover", mouseover)
       .on("mouseleave", mouseleave)
-	
+
+	// show net value symbol & text
+	var netSymbol = d3.symbol().type(d3.symbolSquare).size(10);
+
+	svg.append("g")
+		.selectAll("g")
+		.data(netValues)
+		.enter()
+			.append("path")
+				.attr("d", netSymbol)
+				.attr("transform", function(d) {
+									var xoffset = x(d[0])+x.bandwidth()/2;
+									return "translate("+xoffset+","+y(d[1])+")"; 
+									})
+
+	svg.append("g")
+		.selectAll("g")
+		.data(netValues)
+		.enter()
+			.append("text")
+			.attr("fill", "black")  
+			.attr("font-size", "0.7em")
+			.attr("x", function(d) {
+						var xoffset = x(d[0])+x.bandwidth()/2 - 11;
+						return xoffset; 
+					})
+			.attr("y", function(d,i){ return y(d[1])- 9 }) // 100 is where the first dot appears. 25 is the distance between dots
+			.text(function(d){ return nb2txt_3c(d[1]) })	
+
+	// show legend
+	  var legendHolder = svg.append('g')
+		  .attr("width", legend_width)
+		  .attr("height", legend_height)
+		// translate the holder to the right side of the graph
+		  .attr('transform', "translate("+ xoffset_legend + ", "+ yoffset_legend +")")
+	  // Add one dot in the legend for each name.
+	  
+	  var reversedSubgroups = stages.reverse();
+	  
+	  var legend = legendHolder.selectAll("mydots")
+		.data(reversedSubgroups) // name of categories, in reverse order for stacked legend
+		.enter()
+		.append("circle")
+		  .attr("cx", 100)
+		  .attr("cy", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+		  .attr("r", 7)
+		  .style("fill", function(d){ return color(d)})
+	  // Add one dot in the legend for each name.
+		legend = legendHolder.selectAll("mylabels")
+			.data(reversedSubgroups)
+			.enter()
+			.append("text")
+			  .attr("x", 120)
+			  .attr("y", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+			  .style("fill", function(d){ return color(d)})
+			  .text(function(d){ return d})
+			  .attr("text-anchor", "left")
+			  .style("alignment-baseline", "middle")
+	  
+
 }
 
 function plot_waterfall(id_figure, algebraic_values){
@@ -239,10 +349,10 @@ function plot_waterfall(id_figure, algebraic_values){
 			yAxisWidth = 110,
 			xAxisHeight = 21,
 			chartWidth = 500,
-			xScale = d3.scale.linear(),
-			yScale = d3.scale.ordinal(),
+			xScale = d3.scaleLinear(),
+			yScale = d3.scaleBand(),
 			segmentHeight = 44,
-			segmentPadding = segmentHeight/3,
+			segmentPadding = segmentHeight/4,
 			svg = null,
 			colour_plus = 'pink'//'#f0f8ff',
 			colour_minus = 'lightgreen',
@@ -259,8 +369,8 @@ function plot_waterfall(id_figure, algebraic_values){
 		xScale.domain([minVal, maxVal]);
 		//console.log(stages);
 		yScale.domain(stages);
-		yScale.rangeRoundBands([0, chartHeight], 0.1);
-		//yScale.range([0, chartHeight]);
+		yScale.rangeRound([0, chartHeight]).padding(0.3);
+		
 		function setGraphicAttributes(seg, index) {
 			seg.x = xScale(seg.left); //Math.min(seg.startVal, seg.endVal)
 			seg.y = (segmentPadding + segmentHeight) * index;
@@ -276,17 +386,22 @@ function plot_waterfall(id_figure, algebraic_values){
 			
 		}
 		for(i=0; i<segments.length; i++){
-		  setGraphicAttributes(segments[i], i)
+		  setGraphicAttributes(segments[i], i);
+		  console.log("graphic atributes are set for", segments[i].labelX);
 		}
-		//console.log(segments)
+
 		function createSvg(parentElement) {
 			svg = d3.select(parentElement).append("svg");
 			svg.append("g")
 				.attr("class", "chart-group")
 				.attr("transform", "translate(" + (margin.left + yAxisWidth) + "," + margin.top + ")");
 			// setSvgSize
-			svg.attr("width", chartWidth + yAxisWidth + margin.left + margin.right);
-			svg.attr("height", xAxisHeight + chartHeight + margin.top + margin.bottom);
+			//svg.attr("width", chartWidth + yAxisWidth + margin.left + margin.right);
+			//svg.attr("height", xAxisHeight + chartHeight + margin.top + margin.bottom);
+			console.log("width", chartWidth + yAxisWidth + margin.left + margin.right);
+			var svgWidth = chartWidth + yAxisWidth + margin.left + margin.right,
+				svgHeight = xAxisHeight + chartHeight + margin.top + margin.bottom;
+			svg.attr("viewBox", "0 0 "+svgWidth+" "+svgHeight+"");
 		}
 		function wrap(text, width) {
 			text.each(function() {
@@ -316,9 +431,8 @@ function plot_waterfall(id_figure, algebraic_values){
 			var tickValues = [], maxTickVal;
 			svg.select(".x.axis").remove();
 			if (chartHeight > 0 && segments && segments.length > 0) {
-				var xAxis = d3.svg.axis()
-						.scale(xScale)
-						.orient("bottom"),
+				var xAxis = d3.axisBottom()
+						.scale(xScale),
 					minVal = d3.min(segments, function (d) {
 						return d.end;
 					}),
@@ -377,16 +491,16 @@ function plot_waterfall(id_figure, algebraic_values){
 			var padding = -15;
 			svg.select(".y.axis").remove();
 			if ( chartHeight > 0 ) {
-				var yAxis = d3.svg.axis()
-						.scale(yScale)
-						.orient("left")
-						.tickSize(1);
+				var yAxis = d3.axisLeft()
+						.scale(yScale);
+						//.tickSize(1);
 				svg.select(".chart-group")
 					.append("g")
 					.attr("class", "y axis")
 					.attr("transform", "translate(" + padding + ",0)")
 					.call(yAxis)
 					.selectAll(".tick text")
+					.attr("font-size", "1.2em")
 					.call(wrap, yAxisWidth - 10);
 			}
 		}
@@ -405,12 +519,12 @@ function plot_waterfall(id_figure, algebraic_values){
 				.style("fill", d3.rgb(colour_plus).darker(2));
 		}
 		function drawStartingLine() {
+			var lineColor = d3.rgb(colour_plus).darker(2);
 			svg.select(".top.axis").remove();
 			svg.select(".starting-line").remove();
 			if (chartHeight > 0) {
-				var xAxis = d3.svg.axis()
+				var xAxis = d3.axisTop()
 					.scale(xScale)
-					.orient("top")
 					.tickValues([0])
 					.tickFormat(d3.format("d")),
 					x0 = xScale(0),
@@ -424,36 +538,55 @@ function plot_waterfall(id_figure, algebraic_values){
 					.attr("y1", 0)
 					.attr("x2", x0)
 					.attr("y2", chartHeight);
+				// add 0 line (vertical)
+				chartGroup.append("line")
+					.attr("class", "starting-line")
+					.attr("x1", x0)
+					.attr("y1", 0)
+					.attr("x2", x0)
+					.attr("y2", chartHeight)
+					.style("stroke", lineColor)
 			}
 		}
 		function drawWaterfall() {
 			var chartGroup, barGroup,
 				lineColor = d3.rgb(colour_plus).darker(2);
 			chartGroup = svg.select(".chart-group");
+
 			if (segments && segments.length > 0) {
+				console.log("in the draw if");
 				// Draw bars
 				barGroup = chartGroup.selectAll(".bar.g")
 					.data(segments);
 				barGroup.enter().append("g")
 					.attr("class", "bar g");
 				barGroup.exit().remove();
-				barGroup.append("rect")
+				
+				barGroup.enter().append("rect") // v3 to v4 >> add an enter...
+					.merge(barGroup)
 					.attr("class", "bar rect")
 					.attr("height", segmentHeight)
 					.attr("x", function (d) {
+						console.log("d.x", d.x);
 						return d.x;
 					})
 					.attr("y", function (d) {
+						console.log("d.y", d.y);
 						return d.y;
 					})
 					.attr("width", function (d) {
+						console.log("d.width", d.width);
 						return d.width > 0 ? d.width : 0.1;
 					})
 					.style("fill", function(d){return d.value>0 ? colour_plus : colour_minus})//color)
-					.style("stroke", function(d){return d.value>0 ? d3.rgb(colour_plus).darker(2) : d3.rgb(colour_minus).darker(2)});//lineColor);
-				// Label bars
-				barGroup.append("text")
+					.style("stroke", function(d){return d.value>0 ? d3.rgb(colour_plus).darker(2) : d3.rgb(colour_minus).darker(2)})
+					;//lineColor);
+				
+					// Label bars
+				barGroup.enter().append("text") // v3 to v4 >> add an enter...
+					.merge(barGroup) // not needed
 					.attr("class", "bar text")
+					.attr("font-size", "1.0em")
 					.attr("x", function (d) {
 						return d.labelX;
 					})
@@ -468,7 +601,7 @@ function plot_waterfall(id_figure, algebraic_values){
 						return labelFormat(d.value);
 					});
 				// Connect bars
-				barGroup.append("line")
+				barGroup.enter().append("line")
 					.attr("class", "bar line")
 					.attr("x1", function (d) {
 						return d.endX;
@@ -485,6 +618,7 @@ function plot_waterfall(id_figure, algebraic_values){
 					.style("stroke", lineColor);
 			}
 		}
+
 		function drawEndMarker() {
 			var connectors = svg.selectAll(".bar.line");
 			if (connectors.size() > 0) {
