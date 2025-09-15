9b559d3160f489e66fe86c556d61064aa979aaa3
Guillermo de Dios Maeso <guillermo.de.dios.maeso@slu.se>
2025-09-15 10:04:08 +0200

    Rebuild site from dev

--- assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-// Put custom JavaScript code in this file
-
-// Initialise all the tooltips (for their styling)
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
--- content/sv/gallery-pyrolysis/index.md
+++
@@ -1,4 +0,0 @@
----
-headless: true
----
-Note: you need to duplicate the “index.md” file for every language; but the other languages will inherit the resources if not present 
